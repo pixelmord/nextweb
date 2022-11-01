6b3e108fed66373a0d5c8c40f6a99a40d7ac7504
Andreas Adam <andreas.adam@composableweb.com>
2022-11-01 17:41:00 +0100

    feat: use google fonts

--- apps/composableweb/app/layout.tsx
+++ apps/composableweb/app/layout.tsx
@@ -1,12 +1,14 @@
+import { Archivo_Narrow, Barlow } from '@next/font/google'
 import '../styles/global.css'
+const archivo = Archivo_Narrow({subsets: ['latin']})
+const barlow = Barlow({weight: '400', subsets: ['latin']})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html>
-      <head></head>
+    <html className={`${archivo.className} ${barlow.className}`}>
       <body>{children}</body>
     </html>
   )
---
+++ apps/pixelmord/app/layout.tsx
@@ -0,0 +1,15 @@
+import { Lato } from '@next/font/google'
+import '../styles/global.css'
+
+const lato = Lato({weight: ['400', '700'], subsets: ['latin']})
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html className={`${lato.className}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
