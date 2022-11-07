7a02ad97e67ffcf8ed229ce200a8ceabbaeab4ac
Andreas Adam <andreas.adam@composableweb.com>
2022-11-07 18:32:58 +0100

    fix: typos and props

--- packages/ui/Typography.tsx
+++ packages/ui/Typography.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-export const H1: React.FC = ({children, ...rest}) => {
+export const H1: React.FC<React.PropsWithChildren> = ({children, ...rest}) => {
   return <h1 className="text-3xl font-bold">{children}</h1>;
 };
