146a137c7bdf90f8e817d3a15a258715bb6a2b7a
Andreas Adam <andreas.adam@composableweb.com>
2022-11-10 16:35:26 +0100

    feat: POO-16 sources for all content types

--- apps/composableweb/lib/mdx-sources.ts
+++ apps/composableweb/lib/mdx-sources.ts
@@ -10,7 +10,48 @@ export const Blog = createSource({
     title: z.string(),
     createdAt: z.string(),
     publishedAt: z.string(),
-    excerpt: z.string().optional(),
+    summary: z.string().optional(),
+    draft: z.boolean(),
+  }),
+});
+
+export const CodeRecipes = createSource({
+  contentPath: 'content/code-recipes',
+  basePath: '/code-recipes',
+  sortBy: 'createdAt',
+  sortOrder: 'desc',
+  frontMatter: z.object({
+    title: z.string(),
+    createdAt: z.string(),
+    publishedAt: z.string(),
+    summary: z.string().optional(),
+    draft: z.boolean(),
+  }),
+});
+
+export const Knowhow = createSource({
+  contentPath: 'content/knowhow',
+  basePath: '/knowhow',
+  sortBy: 'createdAt',
+  sortOrder: 'desc',
+  frontMatter: z.object({
+    title: z.string(),
+    createdAt: z.string(),
+    publishedAt: z.string(),
+    summary: z.string().optional(),
+    draft: z.boolean(),
+  }),
+});
+export const Pages = createSource({
+  contentPath: 'content/pages',
+  basePath: '/pages',
+  sortBy: 'createdAt',
+  sortOrder: 'desc',
+  frontMatter: z.object({
+    title: z.string(),
+    createdAt: z.string(),
+    publishedAt: z.string(),
+    summary: z.string().optional(),
     draft: z.boolean(),
   }),
 });
