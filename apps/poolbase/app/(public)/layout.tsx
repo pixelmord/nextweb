b94c9bc3d1eeb683c8676f2b82548fed3c4eea66
Andreas Adam <andreas.adam@composableweb.com>
2022-11-29 12:35:18 +0100

    debug: no session on ssr

--- apps/poolbase/app/(public)/layout.tsx
+++ apps/poolbase/app/(public)/layout.tsx
@@ -1,16 +1,13 @@
 import Link from 'next/link';
 import MainNavigation from './MainNavigation';
-import { fetchUserProfile, setSession } from '@/lib/ssrApi';
+// import { fetchUserProfile, setSession } from '@/lib/ssrApi';
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const data = await setSession();
-  let user = null;
-  if (data?.session) {
-    user = await fetchUserProfile();
-  }
+  // const data = await setSession();
+  // const user = await fetchUserProfile();
 
   return (
     <>
-      <MainNavigation user={user} />
+      <MainNavigation user={null} />
       <main className="mx-auto max-w-7xl px-4 sm:px-6">{children}</main>
 
       <footer className="mx-auto max-w-7xl px-4 sm:px-6">
