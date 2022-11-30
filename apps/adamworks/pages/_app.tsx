759104d9a014db0f18c8ded865b6ff3e867ce4c0
Andreas Adam <andreas.adam@composableweb.com>
2022-11-30 18:54:50 +0100

    feat: use google font on adamworks

--- apps/adamworks/pages/_app.tsx
+++ apps/adamworks/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { Lora } from '@next/font/google';
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
+const lora = Lora({ subsets: ['latin'], variable: '--font-lora' });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${lora.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
