4958a89dd582f6197b1a9fe7a7001f25b9f73a69
Andreas Adam <andreas.adam@composableweb.com>
2023-03-01 15:00:27 +0100

    feat: grainy background

--- apps/poolbase/src/app/(restricted)/layout.tsx
+++ apps/poolbase/src/app/(restricted)/layout.tsx
@@ -1,3 +1,3 @@
 export default async function RestrictedLayout({ children }: React.PropsWithChildren) {
-  return <>{children}</>;
+  return <div className="flex flex-col flex-grow grainy-bg dark:bg-base-900/75 bg-base-100/75">{children}</div>;
 }
